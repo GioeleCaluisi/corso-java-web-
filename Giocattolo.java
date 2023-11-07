3941e21a63db6c6794b0afebec1837b754476ec8
GioeleCalusi <gcaluisi05@gmail.com>
2023-11-07 16:16:11 +0100

    creazione classi cliente e giocattolo

---
+++ Giocattolo.java
@@ -0,0 +1,58 @@
+public class Giocattolo {
+
+   
+    private static Integer id=1;
+    private String nome;
+    private double prezzo;
+    private int etaConsigliata;
+
+
+     public Giocattolo(int id, String nome, double prezzo, int etaConsigliata) {
+        id++;
+        this.nome = nome;
+        this.prezzo = prezzo;
+        this.etaConsigliata = etaConsigliata;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+
+    public int getEtaConsigliata() {
+        return etaConsigliata;
+    }
+
+
+    public void setEtaConsigliata(int etaConsigliata) {
+        this.etaConsigliata = etaConsigliata;
+    }
+    
+    
+}
---
+++ Vendita.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vendita {
+
+    List<Giocattolo> giocattoli = new ArrayList<>(); 
+
+    static void registroAcquisti(){
+        
+    }
+    
+    
+}
